0f68e442d8f7e58f6d33611938c20c64c4fea282
Rashmi Jha <749rashmijha@gmail.com>
2025-09-15 22:51:35 +0530

    ISM-Task 1 DSA Lab.py

--- ISM-Task 1 DSA Lab.py
+++ ISM-Task 1 DSA Lab.py
@@ -1,3 +1,5 @@
+# Real-World Problem Context: This project simulates a real-life inventory system used in shops or warehouses. It helps track stock levels, identify missing or zero-stock items, and manage products efficiently using arrays. Such systems are crucial in ensuring smooth operations, avoiding overstocking or shortages and improving overall inventory management in businesses.
+--CODE--
 inventory=[]
 def insert_product():
     sku = input("Enter SKU: ")
@@ -113,4 +115,5 @@ def main():
             break
         else:
             print("Invalid choice. Please try again.")
+
 main()
